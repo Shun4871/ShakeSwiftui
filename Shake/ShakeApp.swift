fb17e988880d21e5e615ce93d76b7a80260cbbca
ShunnosukeTsuge <shuntsugework@gmail.com>
2025-07-01 23:06:25 +0900

    Initial Commit

---
+++ Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 柘植俊之介 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
